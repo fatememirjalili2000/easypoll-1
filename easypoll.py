113d58f093cf4fc60b29e06122039a027c46af13
Fatima <fatemehmirjalili2000@gmail.com>
2022-11-22 21:05:48 +0330

    first commit

--- easypoll.py
+++ easypoll.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 import discord
 import os
@@ -9,51 +7,20 @@ import re
 from dataclasses import dataclass
 from dotenv import load_dotenv
 from typing import List, Dict, Optional
-
-__author__ = "Adrien Lescourt"
-__email__ = "adrien.lescourt@hesge.ch"
-__copyright__ = "2020, HES-SO"
-__license__ = "GNU GPL v3.0"
-__status__ = "Dev"
-__version__ = "0.2"
-
-
-"""
-Discord bot to creates polls
-
-The question must comes as first argument after the /poll
-All arguments must comes between double quotes
-You can optionally add choices.
-
-/poll "Question"
-or
-/poll "Question" "Choice A" "Choice B" "Choice C"
-"""
-
-
+ 
 REGEX = re.compile(r'"(.*?)"')
-
-
 class PollException(Exception):
     pass
+
 
 
 @dataclass
 class Poll:
     question: str
     choices: List[str]
-
+    
     @classmethod
     def from_str(cls, poll_str: str) -> "Poll":
-        """Return a Poll object from a string that match this template:
-
-        '/poll "Question comes first" "then first choice" "second choice" "third choice"'     end so on if needed
-
-        or simpler question that need binary answer:
-        '/poll "Only the question"
-
-        Raises PollException if the double quotes count is odd
-        """
         quotes_count = poll_str.count('"')
         if quotes_count == 0 or quotes_count % 2 != 0:
             raise PollException("Poll must have an even number of double quotes")
@@ -66,13 +33,9 @@ class Poll:
         return "📊 " + self.question
 
     def get_embed(self) -> Optional[discord.Embed]:
-        """Construct the nice and good looking discord Embed object that represent the poll choices
-
-        returns None if there is no choice for this question (yes/no answer)
-        The reason we put answer choices in the embed but not the question: embed can not display @mentions
-        """
         if not self.choices:
             return None
+        #sakhtan e choice ha
         description = "\n".join(
             self.get_regional_indicator_symbol(idx) + " " + choice
             for idx, choice in enumerate(self.choices)
@@ -84,6 +47,7 @@ class Poll:
 
     def reactions(self) -> List[str]:
         """Add as many reaction as the Poll choices needs"""
+        print(self.choices ,"dddd")
         if self.choices:
             return [
                 self.get_regional_indicator_symbol(i) for i in range(len(self.choices))
@@ -100,16 +64,12 @@ class Poll:
 
 
 class EasyPoll(discord.Client):
-    """Simple discord bot that creates poll
-
-    Each time a poll is send, we store it in a dict with the message nonce as key.
-    When the bot read one of its own message, it checks if the nouce is in the dict
-    If yes, it add the poll reactions emoji to the message
-    """
-
-    def __init__(self, **options):
-        super().__init__(**options)
+    def __init__(self,intents=discord.Intents.default()):
+        intents.message_content = True
+        super().__init__(intents=intents)
         self.polls: Dict[int, Poll] = {}
+        for i in self.polls:
+            print(i)
 
     @staticmethod
     def help() -> discord.Embed:
@@ -119,7 +79,7 @@ class EasyPoll(discord.Client):
         """
         embed = discord.Embed(
             title="Usage:", description=description, color=discord.Color.dark_red()
-        )
+        ).setImage("https://images-ext-2.discordapp.net/external/cC-YBJkH2GXnX7MHMASUM9Gle1S1im3rDJj2K54A28w/%3Fcid%3D73b8f7b19a5ccc575679c0a7fc4a673b753e4ce993f35223%26rid%3Dgiphy.mp4/https/media2.giphy.com/media/Q8bEDnj9hZd6vivXSZ/giphy.mp4").setThumbnail("https://images-ext-2.discordapp.net/external/cC-YBJkH2GXnX7MHMASUM9Gle1S1im3rDJj2K54A28w/%3Fcid%3D73b8f7b19a5ccc575679c0a7fc4a673b753e4ce993f35223%26rid%3Dgiphy.mp4/https/media2.giphy.com/media/Q8bEDnj9hZd6vivXSZ/giphy.mp4")
         embed.set_footer(text="HEPIA powered")
         return embed
 
@@ -130,9 +90,14 @@ class EasyPoll(discord.Client):
 
     async def send_reactions(self, message: discord.message) -> None:
         """Add the reactions to the just sent poll embed message"""
+        for i in range(len(self.polls)):
+            print(i)
         poll = self.polls.get(message.nonce)
+        print(poll)
         if poll:
             for reaction in poll.reactions():
+                print(reaction)
+                print(poll.reactions)
                 await message.add_reaction(reaction)
             self.polls.pop(message.nonce)
 
@@ -140,27 +105,60 @@ class EasyPoll(discord.Client):
         """Send the embed poll to the channel"""
         poll = Poll.from_str(message.content)
         nonce = random.randint(0, 1e9)
+    
         self.polls[nonce] = poll
+
         await message.delete()
-        await message.channel.send(poll.get_message(), embed=poll.get_embed(), nonce=nonce)
+        await message.channel.send(poll.get_message(), embed=poll.get_embed() , nonce=nonce)
+        await message.
+        await react_message.add_reaction(reaction)
+        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
+        await react_message.edit_message(embed=embed)
 
     async def on_message(self, message: discord.message) -> None:
         """Every time a message is send on the server, it arrives here"""
-
-        if message.author == self.user:
-            await self.send_reactions(message)
-            return
-
         if message.content.startswith("/poll"):
             try:
                 await self.send_poll(message)
             except PollException:
                 await message.channel.send(embed=self.help())
+        await self.send_reactions(message)
+        return
 
 
 if __name__ == "__main__":
     
     load_dotenv()
-    token = os.getenv("DISCORD_TOKEN")
+    # token = os.getenv("DISCORD_TOKEN")
+    # client = EasyPoll()
+    # client.run(token)
+
     client = EasyPoll()
-    client.run(token)
+    client.send_reactions
+    client.run("MTAzMjU5MzEyMzI0MDU3OTA4Mg.G4-wx1.4ArGj9GacU9UQUk096_c4TMtOjm-py5thCjbFs")
+
+
+
+# import discord
+
+# intents = discord.Intents.default()
+# intents.message_content = True
+
+# client = discord.Client(intents=intents)
+
+# @client.event
+# async def on_ready():
+#     print(f'We have logged in as {client.user}')
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+
+# client.run("MTAzMjU5MzEyMzI0MDU3OTA4Mg.GSujz1.funzlGUU0YhJkZoirj1hiJGgEgFCK8C5UWVs4M")
+
+
+# # 
